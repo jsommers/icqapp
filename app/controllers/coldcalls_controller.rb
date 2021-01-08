37db4cb1f0de1f2f78ab42962bf24e2d4e241093
Joel Sommers <jsommers@colgate.edu>
2021-01-08 12:14:19 -0500

    add page for manua editing of cold calls

--- app/controllers/coldcalls_controller.rb
+++ app/controllers/coldcalls_controller.rb
@@ -8,6 +8,30 @@ class ColdcallsController < ApplicationController
 
   def update
     @course = Course.find(params[:course_id])
+    user = user_param
     @coldcalls = ColdCall.where(course: params[:course_id])
+    cc = ColdCall.where(course: params[:course_id], user: user).first
+    count = count_param
+    cc.count = count
+    if cc.save
+      flash[:notice] = "Cold call count updated for #{user_email}"
+      render 'edit'
+    else
+      flash[:alert] = "Error saving updated count for user #{user_email}"
+      render 'edit'
+    end
+  end
+
+private
+  def user_param
+    params.require(:user)
+  end
+
+  def user_email
+    params.require(:email)
+  end
+
+  def count_param
+    params.require(:count)
   end
 end
