c25d75d5ccb8818aa7f6dea74c814b82171c71fb
Joel Sommers <jsommers@colgate.edu>
2023-05-15 16:14:22 -0400

    add stuff

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, 
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  validates :email, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :courses
+  has_and_belongs_to_many :attendances
+  has_many :poll_responses, :dependent => :destroy
+  has_many :polls, :through => :poll_responses
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+  end
+
+  def student?
+    return !self.admin
+  end
+end
