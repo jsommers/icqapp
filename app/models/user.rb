7f98d4e79357fa07c8ba7cedc09b15d0972af3d0
Joel Sommers <jsommers@colgate.edu>
2023-05-15 20:37:15 -0400

    some fixing/refactoring for rails 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :email, presence: true, uniqueness: true
-
   has_and_belongs_to_many :courses
   #has_and_belongs_to_many :attendances
   #has_many :poll_responses, :dependent => :destroy
   #has_many :polls, :through => :poll_responses
+
+  scope :students, ->() { where(admin: false).order(:email) }
+  scope :instructors, ->() { where(admin: true).order(:email) }
 
   def self.from_omniauth(access_token)
     data = access_token.info
