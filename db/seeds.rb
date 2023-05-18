f2ddb1db62945bfd560e4b68cbd19a782985bcbe
Joel Sommers <jsommers@colgate.edu>
2023-05-18 17:38:30 -0400

    coldcall turbo

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ Course.destroy_all
 
 i = User.create!(:email => 'jsommers@colgate.edu', :admin => true)
 s = User.create!(:email => 'sommersmeister@gmail.com', :admin => false)
+s2 = User.create!(:email => 'jsommerscymru@gmail.com', :admin => false)
 
 c415 = Course.create!(:name => 'COSC415F23', :daytime => 'MWF 10:20-11:10')
 c415.students << s
+c415.students << s2
 c415.instructors << i
