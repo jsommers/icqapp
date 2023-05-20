1bd58244de9565490082d5df924d5b9edc572a70
Joel Sommers <jsommers@colgate.edu>
2023-05-20 12:03:23 -0400

    working on active question update

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,7 +5,22 @@ class Poll < ApplicationRecord
 
   validates_associated :question
 
+  # on create/update: broadcast to active question
+  after_create_commit :activate_question
+  after_update_commit :deactivate_question
+
   def self.closeall(course)
     Poll.joins(:question).where("polls.isopen = ? AND polls.question_id = questions.id AND questions.course_id = ?", true, course.id).update_all(:isopen => false)
+  end
+
+
+  protected
+  def activate_question
+    pr = self.new_response.save
+    broadcast_replace_later_to 'active_question', partial: 'courses/activequestion', target: 'activequestion', locals: {course: self.question.course, question: self.question, poll: self, poll_response: pr, activepoll: true}
+  end
+
+  def deactivate_question
+    broadcast_replace_later_to 'active_question', partial: 'courses/activequestion', target: 'activequestion', locals: {course: self.question.course, question: self.question, poll: self, activepoll: false}
   end
 end
